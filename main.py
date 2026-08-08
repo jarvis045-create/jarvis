4a3f2a6018ac6bcdf800f56a4d0af0cc7a23606b
jarvis045-create <zseymur135@gmail.com>
2026-08-09 03:14:51 +0400

    masaustu modulleri devre disi birakildi

--- main.py
+++ main.py
@@ -25,12 +25,12 @@ from memory.memory_manager import load_memory, update_memory, delete_memory, for
 from sys_info  import sys_info
 from calendar_actions import get_calendar_events, add_calendar_event, delete_calendar_event
 from reminders import get_reminders, add_reminder
-from browser   import browser_control
-from shell     import shell_run
+#from browser   import browser_control
+#from shell     import shell_run
 #from whatsapp  import send_whatsapp_message, save_whatsapp_contact
-from media     import play_media
+#from media     import play_media
 from weather   import get_weather_summary
-from screen_vision import analyze_screen
+#from screen_vision import analyze_screen
 from youtube_stats import get_youtube_channel_report
 
 try:
